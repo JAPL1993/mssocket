e0d9dc4e2d25f5babf1927319c6fd32e1141a94b
KevinDavila <kevindavila.developer@gmail.com>
2023-04-19 15:05:14 -0600

    cambio de urls para produccion y socket para produccion

--- src/http-axios/http-axios/http-axios.service.ts
+++ src/http-axios/http-axios/http-axios.service.ts
@@ -6,7 +6,7 @@ import axios, { AxiosResponse } from 'axios';
 export class HttpAxiosService {
   constructor(private readonly httpService: HttpService) {}
   postNode(endpoint: string, data: any): Promise<AxiosResponse> {
-    return axios.post(`http://localhost:8081/${endpoint}`, data, {
+    return axios.post(`http://162.214.164.60:8082/${endpoint}`, data, {
       headers: {
         Authorization:
           'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiVG9kb3MiLCJpZCI6MjksImlhdCI6MTY2Mzg4ODg2NSwiZXhwIjoxNjYzODg4ODY4fQ.gDR4G_sB7dfd9jZEf6e0kfgzU0uMxdfB6gajRNINmAw',
@@ -14,6 +14,6 @@ export class HttpAxiosService {
     });
   }
   postMicrosip(endpoint: string, data: any): Promise<AxiosResponse> {
-    return axios.post(`http://192.168.10.133:442/${endpoint}`, data);
+    return axios.post(`http://192.168.10.210:8080/${endpoint}`, data);
   }
 }
